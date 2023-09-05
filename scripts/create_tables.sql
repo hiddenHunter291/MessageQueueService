aa6924b4153304da21132a69c9bf7ca52a0a891c
sivahari <sivaharias@tekion.com>
2023-09-05 19:27:55 +0530

    sql script added to create tables(product)

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -7,8 +7,48 @@ CREATE TABLE `users`
     `mobile_number` bigint(20) NOT NULL,
     `longitude` Double precision(10,4) NOT NULL,
     `latitude` Double precision(10,4) NOT NULL,
-    `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-    `updated_at` timestamp,
-    PRIMARY KEY (`id`)
+    `created_at` bigint(20) NOT NULL,
+    `updated_at` bigint(20) NOT NULL,
+    primary key (`id`)
+);
+
+CREATE TABLE `products`
+(
+    `id` bigint(20) NOT NULL auto_increment,
+    `product_name` varchar(100) NOT NULL,
+    `product_description` varchar(250) NOT NULL,
+    `product_price` bigint(20) NOT NULL,
+    `created_at` bigint(20) NOT NULL,
+    `updated_at` bigint(20) NOT NULL,
+    primary key (`id`)
+);
+
+CREATE TABLE `orders`
+(
+    `id` bigint(20) NOT NULL auto_increment,
+    `user_id` bigint(20) NOT NULL,
+    `product_id` bigint(20) NOT NULL,
+    foreign key (`user_id`) references users(`id`),
+    foreign key (`product_id`) references products(`id`),
+    primary key (`id`)
+);
+
+
+CREATE TABLE `product_links`
+(
+    `id` bigint(20) NOT NULL auto_increment,
+    `product_id` bigint(20) NOT NULL,
+    `link` varchar(200) NOT NULL,
+    foreign key (`product_id`) references products(`id`),
+    primary key (`id`)
+);
+
+CREATE TABLE `compressed_product_path`
+(
+    `id` bigint(20) NOT NULL auto_increment,
+    `product_id` bigint(20) NOT NULL,
+    `path` varchar(200) NOT NULL,
+    foreign key (`product_id`) references products(`id`),
+    primary key (`id`)
 );
 
